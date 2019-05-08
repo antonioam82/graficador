6385ec0ca086ed154a4e8e9397ac203f582b6df1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-08 15:45:52 +0200

    Update graficador.py

--- graficador.py
+++ graficador.py
@@ -12,7 +12,7 @@ from math import *
 
 root = tkinter.Tk()
 root.wm_title("Graficador")
-ta=root.geometry("1000x700")
+ta=root.geometry("1000x700")#1000x700
 #root.configure(background="SkyBlue4")
 
 style.use('fivethirtyeight')
@@ -25,8 +25,8 @@ canvas = FigureCanvasTkAgg(fig, master=root)  # CREAR AREA DE DIBUJO DE TKINTER.
 canvas.draw()
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-#toolbar = NavigationToolbar2Tk(canvas, root)# barra de iconos
-#toolbar.update()
+toolbar = NavigationToolbar2Tk(canvas, root)# barra de iconos
+toolbar.update()
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 act_rango=False
 ul_ran=""
@@ -38,25 +38,30 @@ def animate(i):
         try:
             lmin = float(ran[0]); lmax = float(ran[1])
             if lmin < lmax:
-                x = np.arange(lmin, lmax, .01)
+                x = np.arange(lmin, lmax, .01)#.01
                 ul_ran = [lmin, lmax]
             else:
                 act_rango = False
         except:
             messagebox.showwarning("Error","Entrada no válida")
+            #print("Se repite")
             act_rango=False
             ets.delete(0,len(ets.get()))
     else:
         if ul_ran!="":
-            x = np.arange(ul_ran[0],ul_ran[1], .01)
+            x = np.arange(ul_ran[0],ul_ran[1], .01)#.01
         else:
-            x = np.arange(1, 10, .01)
+            x = np.arange(1, 10, .01)#.01
     try:
         solo=eval(graph_data)
         ax1.clear()
         ax1.plot(x,solo)
+        ax1.axhline(0, color="black")
+        ax1.axvline(0, color="black")
     except:
         ax1.plot()
+        ax1.axhline(0, color="black")
+        ax1.axvline(0, color="black")
 
 def represent():
     global graph_data
@@ -80,7 +85,7 @@ ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
 
 et =tkinter.Entry(master=root,width=60)
-et.config(bg="black", fg="#03f943", justify="left")
+#et.config(bg="black", fg="#03f943", justify="left")
 et.pack(side=tkinter.TOP)
 button.pack(side=tkinter.BOTTOM)
 
@@ -89,4 +94,3 @@ ets.pack(side=tkinter.RIGHT)
 
 
 tkinter.mainloop()
-
