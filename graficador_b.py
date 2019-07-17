5283c311ff70b61565d434e88283d3fc267d1ed2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-07-17 23:37:43 +0200

    Update graficador_b.py

--- graficador_b.py
+++ graficador_b.py
@@ -32,6 +32,15 @@ canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 act_rango=False
 ul_ran=""
 ran=""
+
+funciones={"sin":"np.sin","cos":"np.cos","tan":"np.tan","log":"np.log",
+           "pi":"np.pi","sqrt":"np.sqrt"}
+
+def reemplazo(s):
+    for i in funciones:
+        if i in s:
+            s=s.replace(i, funciones[i])
+    return s
     
 def animate(i):
     global ejes
@@ -56,30 +65,36 @@ def animate(i):
         else:
             x = np.arange(1, 10, .01)#.01
     try:
-        solo=eval(graph_data)
-        ax1.clear()
-        ax1.plot(x,solo)
+        if graph_data2!="":
+            solo2=eval(graph_data2)
+            solo=eval(graph_data)
+            ax1.clear()
+            ax1.plot(x,solo,x,solo2)
+        else:
+            solo=eval(graph_data)
+            ax1.clear()
+            ax1.plot(x,solo)
     except:
         ax1.plot()
     ax1.axhline(0, color="gray")
     ax1.axvline(0, color="gray")
+    ani.event_source.stop()
 
 def represent():
     global graph_data
     global ran
     global act_rango
+    global graph_data2
     texto_orig=et.get()
+    texto_orig2=etn.get()
     if ets.get()!="":
         rann=ets.get()
         ran=rann.split(",")
         act_rango=True
-    ta=texto_orig.replace("sin","np.sin")
-    tb=ta.replace("cos","np.cos")
-    tl=tb.replace("log","np.log")
-    tc=tl.replace("tan","np.tan")
-    tr=tc.replace("sqrt","np.sqrt")
-    graph_data=tr
-
+    graph_data=reemplazo(texto_orig)
+    graph_data2=reemplazo(texto_orig2)
+    ani.event_source.start()
+    
 def guardar(e):
     global expresiones
     expresiones.append(e)
@@ -89,16 +104,18 @@ def guardar(e):
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
 
-et = tkinter.Entry(master=root,width=60)
-et.config(bg="gray87", justify="left")
+etn = tkinter.Entry(master=root,width=60)
+etn.config(bg="gray87", justify="left")
 button = tkinter.Button(master=root, text="SET", bg="gray69", command=represent)
 button.pack(side=tkinter.BOTTOM)
-et.pack(side=tkinter.BOTTOM)
+etn.pack(side=tkinter.BOTTOM)
 #button = tkinter.Button(master=root, text="VER EJES", command=marca_ejes)
 #button.pack(side=tkinter.LEFT)
 
 ets=tkinter.Entry(master=root,width=10)
 ets.pack(side=tkinter.RIGHT)
+et=tkinter.Entry(master=root,width=60)
+et.place(x=317,y=620)
 label = tkinter.Label(master = root, text = "RANGO DE 'X'")
 label.pack(side = tkinter.RIGHT)
 button = tkinter.Button(master=root, text="GUARDAR FUNCIÓN", bg="gray69", command=lambda:guardar(et.get()))
